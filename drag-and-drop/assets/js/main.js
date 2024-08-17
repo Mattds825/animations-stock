0e192a1688b413a057c1cffbb64f00218984679a
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-08-17 21:18:00 +0100

    add drag and drop  functionality to drag and drop

--- drag-and-drop/assets/js/main.js
+++ drag-and-drop/assets/js/main.js
@@ -3,27 +3,67 @@ let selectedColor = document.querySelector("#colorInput");
 let createBtn = document.getElementById("createBtn");
 let list = document.querySelector("#list");
 
-// let closeBtn = document.querySelector(".close");
-
-// closeBtn.onclick = () => {
-//   console.log("closeBtn.onclick");
-// //   closeBtn.parentElement.remove();
-// };
-
 createBtn.onclick = () => {
-    console.log("createBtn.onClick");
-    let newNote = document.createElement("div");
-    newNote.classList.add("note");
-    newNote.innerHTML = `
+  console.log("createBtn.onClick");
+  let newNote = document.createElement("div");
+  newNote.classList.add("note");
+  newNote.innerHTML = `
       <span class="close">x</span>
       <textarea placeholder="Write Content ..." rows="10" cols="30"></textarea>
       `;
-      newNote.style.borderColor = selectedColor.value;
-    list.appendChild(newNote);
+  newNote.style.borderColor = selectedColor.value;
+  list.appendChild(newNote);
 };
 
 document.addEventListener("click", (e) => {
-    if(e.target.classList.contains("close")) {
-           e.target.parentElement.remove();
+  if (e.target.classList.contains("close")) {
+    e.target.parentElement.remove();
+  }
+});
+
+let cursorPostion = { x: null, y: null };
+
+let note = {
+  dom: null,
+  x: null,
+  y: null,
+};
+
+document.addEventListener("mousedown", (e) => {
+  if (e.target.classList.contains("note")) {
+    cursor = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+    note = {
+        dom: e.target,
+        x: e.target.getBoundingClientRect().left,
+        y: e.target.getBoundingClientRect().top,
     }
+  }
+});
+
+document.addEventListener("mousemove", (e) => {
+    if (note.dom == null) return;
+
+    let currentCursor = {
+        x: e.clientX,
+        y: e.clientY,
+    };
+    
+    let distance = {
+        x: currentCursor.x - cursor.x,
+        y: currentCursor.y - cursor.y,
+    };
+
+    note.dom.style.left = note.x + distance.x + "px";   
+    note.dom.style.top = note.y + distance.y + "px";
+    note.dom.style.cursor = "grab";
+});
+
+
+document.addEventListener("mouseup", (e) => {
+    if (note.dom == null) return;
+    note.dom.style.cursor = "auto";
+    note.dom = null;
 });
